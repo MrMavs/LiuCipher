0988e7c1dc9fe4c60ba73780fd6247287ddbd5ab
MrMavs <mrmavs@hmamail.com>
2015-04-20 20:52:34 +0000

    Create default.pp

---
+++ manifests/default.pp
@@ -0,0 +1,15 @@
+$basedir = '/home/vagrant'
+$mainuser = 'vagrant'
+$maingroup = 'vagrant'
+
+package { 'python-numpy':
+ensure => installed,
+}
+
+package { 'ipython':
+ensure => installed,
+}
+
+package { 'libxml2-utils':
+ensure => installed,
+}
